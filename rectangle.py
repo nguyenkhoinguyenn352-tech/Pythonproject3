26d27c63ab0412052d66735473cdb113f2d68dbe
Home <nguyenkhoinguyenn352@gmail.com>
2025-09-27 21:11:37 +0700

    Add rectangle.py to calculate area and perimeter

--- rectangle.py
+++ rectangle.py
@@ -8,4 +8,5 @@ area = length * width
 
 # Print results
 print("Perimeter of the rectangle is:", perimeter)
-print("Area of the rectangle is:", area)
+print("Area of the rectangle is:",
+      area)
