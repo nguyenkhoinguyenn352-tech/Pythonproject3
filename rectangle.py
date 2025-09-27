cead2a86017f6cd1961165614a3d8120a538cb65
Home <nguyenkhoinguyenn352@gmail.com>
2025-09-27 21:05:06 +0700

    rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+# Ask for the length and width of the rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate perimeter and area
+perimeter = 2 * (length + width)
+area = length * width
+
+# Print results
+print("Perimeter of the rectangle is:", perimeter)
+print("Area of the rectangle is:", area)
